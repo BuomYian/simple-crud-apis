58d01860b0a7e9806578cf1df919fa309c3cfa92
Buomkuoth Yian <buom.nyajal@gmail.com>
2024-06-13 11:11:18 +0300

    Done with GET and POST apis

--- server.js
+++ server.js
@@ -1,25 +1,50 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Product from "./models/product.model.js";
+
+dotenv.config(); // Load environment variables
 
 const app = express();
-
-dotenv.config();
+app.use(express.json()); // Parse JSON bodies
 const mongodb_URL = process.env.MONGO_URL;
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API!");
 });
 
+// Creating a product
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get all products
+app.get("/api/products", async (req, res) => {
+  try {
+    const product = await Product.find({});
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get a product by id
+
 mongoose
   .connect(mongodb_URL)
   .then(() => {
     console.log("Connected to the database");
+    const PORT = 3000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
-  .catch(() => {
-    console.log("Connection failed");
+  .catch((error) => {
+    console.error("Connection failed", error);
   });
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
